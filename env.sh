79d8a7813161cb487475ec0c66fdf2c044043653
Ivan Di Luccio <ivan.diluccio@pagopa.it>
2023-10-10 20:25:34 +0200

    feat(CHK-1967): testing code review pipeline

---
+++ env.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Recreate config file and assignment
+echo "window._env_ = {" > ./src/env-config.js
+
+# Loop on environment variables prefixed with
+# checkout_var and add them to env-config.js
+for checkout_var in $(env | grep -Ei '(checkout_|wallet_|api_)'); do
+    varname=$(printf '%s\n' "$checkout_var" | sed -e 's/=.*//')
+    varvalue=$(printf '%s\n' "$checkout_var" | sed -e 's/^[^=]*=//')
+
+    echo "  $varname: \"$varvalue\"," >> ./src/env-config.js
+done
+
+echo "}" >> ./src/env-config.js
