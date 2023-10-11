27ddfdfebcb4575df0f5c037db520f44594ec20e
Simone Infante <simone.infante@pagopa.it>
2023-10-11 10:58:48 +0200

    fix: set 2.8.3 for parcel

--- src/utils/env.tsx
+++ src/utils/env.tsx
@@ -1,9 +1,9 @@
 const {
   API_HOST = "",
-  API_WALLET_BASEPATH,
-  API_PM_BASEPATH,
-  NODE_ENV,
-  API_TIMEOUT
+  API_WALLET_BASEPATH = "",
+  API_PM_BASEPATH = "",
+  NODE_ENV = "",
+  API_TIMEOUT = ""
 } = process.env;
 
 export default {
