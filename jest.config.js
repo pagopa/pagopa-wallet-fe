85551af86c5bc9f0d7980acc7f7c474336facd8b
Pietro Tota <pietro.tota@pagopa.it>
2023-10-26 10:42:06 +0200

    fix: sonar cloud yarn configuration

--- jest.config.js
+++ jest.config.js
@@ -12,8 +12,20 @@ module.exports = {
     ],
     coverageReporters: ["cobertura",'lcov'],
     collectCoverage: true,
+    collectCoverageFrom: [
+        "src/**/*.ts",
+        "!<rootDir>/src/index.ts",
+        "!<rootDir>/src/__tests__/**/*",
+        "!<rootDir>/src/__mocks__/**/*"
+    ],
+    coveragePathIgnorePatterns: ["index.ts"],
+    testResultsProcessor: "jest-sonar-reporter",
     coveragePathIgnorePatterns: [
-        "generated",
-      ],
-    modulePathIgnorePatterns: ["__integration_tests__"]
+        "node_modules",
+        "test-config",
+        ".module.ts",
+        "<rootDir>/src/generated/",
+        "<rootDir>/src/__mocks__/*.ts"
+    ],
+    coverageDirectory: "<rootDir>/coverage/",
   };
