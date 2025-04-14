1bc7f47e86d480d6c5cb985f71bc25175c656614
Simone Infante <52280205+infantesimone@users.noreply.github.com>
2025-04-14 20:26:41 +0200

    chore: update coverage config

--- jest.config.js
+++ jest.config.js
@@ -15,6 +15,28 @@ module.exports = {
       outputName: 'sonar-report.xml',
     } ]
   ],
+  collectCoverage: true,
+  collectCoverageFrom: [
+      "src/**/*.ts",
+      "src/**/*.tsx",
+      "!<rootDir>/src/index.ts",
+      "!<rootDir>/src/instrumentation.ts",
+      "!<rootDir>/src/__tests__/**/*",
+      "!<rootDir>/src/__integration_tests__/**/*",
+      "!<rootDir>/src/__mocks__/**/*"
+  ],
+  coveragePathIgnorePatterns: [
+    "node_modules",
+    "test-config",
+    ".module.ts",
+    "<rootDir>/src/translations",
+    "<rootDir>/src/generated/",
+    "<rootDir>/src/__mocks__/*.ts",
+    "<rootDir>/src/utils/api/pm",
+    "<rootDir>/src/routes/pm",
+    "index.ts",
+    "image.d.ts",
+],
   testResultsProcessor: "jest-sonar-reporter",
   setupFiles: ['<rootDir>/src/env-config.js'],
   modulePathIgnorePatterns: ["__integration_tests__"]
