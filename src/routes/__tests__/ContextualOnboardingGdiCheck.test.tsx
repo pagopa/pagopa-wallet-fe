7af30d85582f90abba18006b8555c6f358378c63
pietro.tota <pietro.tota@pagopa.it>
2025-12-16 16:32:57 +0100

    fix: use replace function

--- src/routes/__tests__/ContextualOnboardingGdiCheck.test.tsx
+++ src/routes/__tests__/ContextualOnboardingGdiCheck.test.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable 
     @typescript-eslint/no-var-requires
 */
-
+/* eslint-disable functional/immutable-data */
 import React from "react";
 import { render, act } from "@testing-library/react";
 import "@testing-library/jest-dom";
@@ -9,17 +9,13 @@ import "@testing-library/jest-dom";
 jest.mock("react-i18next", () => ({
   useTranslation: () => ({ t: (k: string) => k })
 }));
-
+const walletContextualOnboardingEcommerceFeUrl = "https://ecommerce-fe/esito";
 jest.mock("../../config", () => ({
   getConfigOrThrow: () => ({
     WALLET_GDI_CHECK_TIMEOUT: 100,
-    WALLET_CONTEXTUAL_ONBOARDING_ECOMMERCE_FE_OUTCOME_URL: "/esito"
+    WALLET_CONTEXTUAL_ONBOARDING_ECOMMERCE_FE_OUTCOME_URL:
+      walletContextualOnboardingEcommerceFeUrl
   })
-}));
-
-const mockNavigate = jest.fn();
-jest.mock("react-router-dom", () => ({
-  useNavigate: () => mockNavigate
 }));
 
 const mockGetFragments = jest.fn().mockReturnValue({
@@ -49,8 +45,23 @@ jest.mock("../../hooks/useNpgSdk", () => ({
   })
 }));
 
+const originalLocation = window.location;
+beforeAll(() => {
+  Object.defineProperty(window, "location", {
+    configurable: true,
+    value: { ...originalLocation, replace: jest.fn() }
+  });
+});
+
+afterAll(() => {
+  Object.defineProperty(window, "location", {
+    configurable: true,
+    value: originalLocation
+  });
+});
+
 import ContextualOnboardingGdiCheckPage from "../ContextualOnboardingGdiCheck";
-import { WalletRoutes, ROUTE_FRAGMENT } from "../../routes/models/routeModel";
+import { ROUTE_FRAGMENT } from "../../routes/models/routeModel";
 
 describe("ContextualOnboardingGdiCheckPage", () => {
   beforeEach(() => {
@@ -69,8 +80,8 @@ describe("ContextualOnboardingGdiCheckPage", () => {
       jest.advanceTimersByTime(100);
     });
 
-    const expectedPath = `/${WalletRoutes.ESITO}#${ROUTE_FRAGMENT.CLIENT_ID}=IO&${ROUTE_FRAGMENT.TRANSACTION_ID}=tx123&${ROUTE_FRAGMENT.SESSION_TOKEN}=tok123`;
-    expect(mockNavigate).toHaveBeenCalledWith(expectedPath, { replace: true });
+    const expectedPath = `${walletContextualOnboardingEcommerceFeUrl}#${ROUTE_FRAGMENT.CLIENT_ID}=IO&${ROUTE_FRAGMENT.TRANSACTION_ID}=tx123&${ROUTE_FRAGMENT.SESSION_TOKEN}=tok123`;
+    expect(window.location.replace).toHaveBeenCalledWith(expectedPath);
 
     jest.useRealTimers();
   });
